13a39a0145866906ddb0a735fc3eca7d91a08002
Aditya Kumar <interstellaraditya@gmail.com>
2024-04-13 14:19:16 +0530

    Not using any third party library for image loading

---
+++ app/src/main/java/com/example/getimages/utils/LoadImageTask.java
@@ -0,0 +1,37 @@
+package com.example.getimages.utils;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+public class LoadImageTask extends AsyncTask<String, Void, Bitmap> {
+    ImageView imageView;
+
+    public LoadImageTask(ImageView imageView) {
+        this.imageView = imageView;
+    }
+
+    @Override
+    protected Bitmap doInBackground(String... strings) {
+        Bitmap bitmap = null;
+        try {
+            InputStream inputStream = new URL(strings[0]).openStream();
+            bitmap = BitmapFactory.decodeStream(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
+
+    @Override
+    protected void onPostExecute(Bitmap bitmap) {
+        if (bitmap != null) {
+            imageView.setImageBitmap(bitmap);
+        }
+    }
+}
